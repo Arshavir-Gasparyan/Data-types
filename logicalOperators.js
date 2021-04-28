f8ec0df5455bda66d65ee3f1b25d1e14db98b1ba
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-04-28 22:46:07 +0400

    Range between

--- logicalOperators.js
+++ logicalOperators.js
@@ -21,3 +21,8 @@ console.log(1);
 // alert( null || 2 && 3 || 4 );
 // rezult = 3
 console.log(null || (2 && 3) || 4);
+
+// Check the range between
+// Write an if condition to check that age is between 14 and 90 inclusively.
+// “Inclusively” means that age can reach the edges 14 or 90
+// if (age >= 14 && age <= 90)
