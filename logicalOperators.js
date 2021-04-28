f56ffef9c37fb2d50e8b6d2b7741542420b2a674
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-04-28 22:41:15 +0400

    Result of AND

--- logicalOperators.js
+++ logicalOperators.js
@@ -6,3 +6,8 @@ console.log(null || 2 || undefined);
 // What's the result of OR'ed alerts?
 // alert( alert(1) || 2 || alert(3) );
 // rezult = 1, then 2
+
+// What is the result of AND?
+// alert( 1 && null && 2 );
+// result = null
+console.log(1 && null && 2);
