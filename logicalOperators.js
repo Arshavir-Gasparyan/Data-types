96a7fb759fb94c011e8b687956d6e5ded66ab662
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-04-28 22:39:42 +0400

    result of OR,ed alerts

--- logicalOperators.js
+++ logicalOperators.js
@@ -2,3 +2,7 @@
 // alert( null || 2 || undefined );
 // rezult = 2
 console.log(null || 2 || undefined);
+
+// What's the result of OR'ed alerts?
+// alert( alert(1) || 2 || alert(3) );
+// rezult = 1, then 2
